94afbe8b48f287e2d7270ba87b90f1fc7207960d
sakibalhasan74 <mdsakibalhasan677@gmail.com>
2024-05-01 09:50:02 +0600

    new file add and new text

---
+++ src/ts.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ts() {
+  return (
+    <div>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, ratione?</p>
+    </div>
+  );
+}
